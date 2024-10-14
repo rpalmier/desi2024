dc79b34551fc0ab35609878dd29640ca6542790a
Dardo Kuttel <dardo@vertiq.com>
2024-10-14 20:12:03 -0300

    Subida inicial del proyecto

---
+++ src/main/java/tuti/desi/Desi2024Application.java
@@ -0,0 +1,13 @@
+package tuti.desi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desi2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desi2024Application.class, args);
+	}
+
+}
